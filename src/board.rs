a51249cedcb59ba789eb76f0cb01b98619cc4d21
Jeroen Lamain <jeroen.lamain@sioux.eu>
2020-08-10 10:44:09 +0200

    separated board code from main. Fixed clippy warnings

---
+++ src/board.rs
@@ -0,0 +1,122 @@
+extern crate time;
+
+use std::fmt;
+
+const WIDTH: usize = 9;
+const HEIGHT: usize = 9;
+const BLOCKSIZE: usize = 3;
+const NR_OF_BLOCKS: usize = (WIDTH / BLOCKSIZE) * (HEIGHT / BLOCKSIZE);
+const NR_OF_CELLS: usize = WIDTH * HEIGHT;
+
+#[derive(Copy, Clone)]
+pub struct Board {
+    rows: [u32; HEIGHT],
+    columns: [u32; WIDTH],
+    blocks: [u32; NR_OF_BLOCKS],
+    board: [u32; NR_OF_CELLS],
+}
+
+impl Board {
+    pub fn from_string(s: String) -> Board {
+        let mut board = Board {
+            rows: [0; HEIGHT],
+            columns: [0; WIDTH],
+            blocks: [0; NR_OF_BLOCKS],
+            board: [0; NR_OF_CELLS],
+        };
+        let bytes = s.as_bytes();
+
+        for (idx, digit ) in bytes.iter().enumerate() {
+            let b: u8 = digit - 48;
+            board = board.set(idx, b as u32);
+        }
+        board
+    }
+
+    fn is_valid(&self, idx: usize, nr: u32) -> bool {
+        let y = idx / HEIGHT;
+        let bitvalue: u32 = 1 << nr;
+        if (self.rows[y] & bitvalue) != 0 {
+            return false;
+        }
+
+        let x = idx % WIDTH;
+        if (self.columns[x] & bitvalue) != 0 {
+            return false;
+        }
+        let x3: usize = x / BLOCKSIZE;
+        let y3: usize = y / BLOCKSIZE;
+        let off: usize = y3 * BLOCKSIZE + x3;
+        if (self.blocks[off] & bitvalue) != 0 {
+            return false;
+        }
+        true
+    }
+
+    fn is_occupied(&self, idx: usize) -> bool {
+        self.board[idx] != 0
+    }
+
+    fn set(&self, idx: usize, nr: u32) -> Board {
+        let y = idx / HEIGHT;
+        let x = idx % WIDTH;
+
+        let mut ret = *self;
+        ret.rows[y] |= 1 << nr;
+        ret.columns[x] |= 1 << nr;
+
+        let x3: usize = x / BLOCKSIZE;
+        let y3: usize = y / BLOCKSIZE;
+        let off: usize = y3 * BLOCKSIZE + x3;
+        ret.blocks[off] |= 1 << nr;
+        ret.board[idx] = nr;
+
+        ret
+    }
+}
+
+
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut ret = "".to_string();
+        for digit in self.board.iter() {
+            ret.push_str(&digit.to_string());
+        }
+       
+        write!(f, "{}", ret)
+    }
+}
+
+
+pub fn solve(b: Board, start_idx: usize) -> Option<Board> {
+    if start_idx == NR_OF_CELLS {
+        return Some(b);
+    }
+
+    if b.is_occupied(start_idx) {
+        return solve(b, start_idx + 1);
+    }
+
+    let mut nr = 1;
+    loop {
+        let solved = if b.is_valid(start_idx, nr) {
+            let br = b.set(start_idx, nr);
+            solve(br, start_idx + 1)
+        } else {
+            None
+        };
+
+        match solved {
+            None => {
+                nr += 1;
+                if nr > 9 {
+                    break;
+                }
+            }
+            Some(_) => {
+                return solved;
+            }
+        }
+    }
+    None
+}
--- src/main.rs
+++ src/main.rs
@@ -3,118 +3,7 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use time::Instant;
 
-const WIDTH: usize = 9;
-const HEIGHT: usize = 9;
-const BLOCKSIZE: usize = 3;
-const NR_OF_BLOCKS: usize = (WIDTH / BLOCKSIZE) * (HEIGHT / BLOCKSIZE);
-const NR_OF_CELLS: usize = WIDTH * HEIGHT;
-
-#[derive(Copy, Clone)]
-struct Board {
-    rows: [u32; HEIGHT],
-    columns: [u32; WIDTH],
-    blocks: [u32; NR_OF_BLOCKS],
-    board: [u32; NR_OF_CELLS],
-}
-
-impl Board {
-    fn from_string(s: String) -> Board {
-        let mut board = Board {
-            rows: [0; HEIGHT],
-            columns: [0; WIDTH],
-            blocks: [0; NR_OF_BLOCKS],
-            board: [0; NR_OF_CELLS],
-        };
-        let bytes = s.as_bytes();
-        for digit in 0..s.len() {
-            let b: u8 = bytes[digit] - 48;
-            board = board.set(digit, b as u32);
-        }
-
-        return board;
-    }
-
-    fn is_valid(&self, idx: usize, nr: u32) -> bool {
-        let y = idx / HEIGHT;
-        let bitvalue: u32 = 1 << nr;
-        if (self.rows[y] & bitvalue) != 0 {
-            return false;
-        }
-
-        let x = idx % WIDTH;
-        if (self.columns[x] & bitvalue) != 0 {
-            return false;
-        }
-        let x3: usize = x / BLOCKSIZE;
-        let y3: usize = y / BLOCKSIZE;
-        let off: usize = y3 * BLOCKSIZE + x3;
-        if (self.blocks[off] & bitvalue) != 0 {
-            return false;
-        }
-        return true;
-    }
-
-    fn is_occupied(&self, idx: usize) -> bool {
-        return self.board[idx] != 0;
-    }
-
-    fn set(&self, idx: usize, nr: u32) -> Board {
-        let y = idx / HEIGHT;
-        let x = idx % WIDTH;
-
-        let mut ret = *self;
-        ret.rows[y] |= 1 << nr;
-        ret.columns[x] |= 1 << nr;
-
-        let x3: usize = x / BLOCKSIZE;
-        let y3: usize = y / BLOCKSIZE;
-        let off: usize = y3 * BLOCKSIZE + x3;
-        ret.blocks[off] |= 1 << nr;
-        ret.board[idx] = nr;
-
-        return ret;
-    }
-    fn to_string(&self) -> String {
-        let mut ret = "".to_string();
-        for digit in self.board.iter() {
-            ret.push_str(&digit.to_string());
-        }
-        return ret;
-    }
-}
-
-fn solve(b: Board, start_idx: usize) -> Option<Board> {
-    if start_idx == NR_OF_CELLS {
-        return Some(b);
-    }
-
-    if b.is_occupied(start_idx) {
-        return solve(b, start_idx + 1);
-    }
-
-    let mut nr = 1;
-    loop {
-        let mut solved: Option<Board> = None;
-
-        if b.is_valid(start_idx, nr) {
-            let br = b.set(start_idx, nr);
-            solved = solve(br, start_idx + 1);
-        }
-
-        match solved {
-            None => {
-                nr = nr + 1;
-                if nr > 9 {
-                    break;
-                }
-            }
-            Some(_) => {
-                return solved;
-            }
-        }
-    }
-    return None;
-}
+mod board;
 
 fn main() {
     // File hosts must exist in current path before this produces output
@@ -127,11 +16,11 @@ fn main() {
     let mut min:f64 = 10000.0;
     for line in lines.skip(1) {
         let z = line.unwrap();
-        let tokens: Vec<&str> = z.split(",").collect();
+        let tokens: Vec<&str> = z.split(',').collect();
 
         let now = Instant::now();
 
-        let solution = solve(Board::from_string(tokens[0].to_string()), 0);
+        let solution = board::solve(board::Board::from_string(tokens[0].to_string()), 0);
         let elapsed = now.elapsed();
         let f = elapsed.as_seconds_f64();
         if f > max {
@@ -155,7 +44,7 @@ fn main() {
                     );
                 }
                 print!("{}\r", nr);
-                nr = nr + 1;
+                nr += 1;
             }
         }
     }
