a81db5f21e940eb519080919fe1791a883ecaa16
Jeroen Lamain <jeroen.lamain@sioux.eu>
2020-10-14 15:17:57 +0200

    rustfmt code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 extern crate time;
+use rayon::prelude::*;
 use std::fs::File;
 use std::io::{self, BufRead};
 use time::Instant;
-use rayon::prelude::*;
 
 mod board;
 
-fn try_solve( puzzle_str: &str, solution_str: &str)
-{
+fn try_solve(puzzle_str: &str, solution_str: &str) {
     let solution = board::solve(board::Board::from_str(puzzle_str), 0);
 
     match solution {
@@ -37,11 +36,16 @@ fn main() {
     for line in lines.skip(1) {
         let z = line.unwrap();
         let mut tokens = z.split(',');
-        v_all.push( (tokens.next().unwrap().to_string(), tokens.next().unwrap().to_string()));
+        v_all.push((
+            tokens.next().unwrap().to_string(),
+            tokens.next().unwrap().to_string(),
+        ));
     }
     let now = Instant::now();
 
-    v_all.par_iter().for_each(|puzzle|   try_solve(&puzzle.0, &puzzle.1) );
+    v_all
+        .par_iter()
+        .for_each(|puzzle| try_solve(&puzzle.0, &puzzle.1));
 
     println!("Solution time: {} seconds ", now.elapsed().as_seconds_f64());
 }
