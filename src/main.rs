0f55277d4fb5d6b730c9626a6a47dc277756defa
Jeroen Lamain <jeroenlamain@gmail.com>
2021-09-15 08:59:03 +0200

    Added command line filename for fuzzing. Fixed crash on empty line (see example/2.csv)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate time;
 use rayon::prelude::*;
+use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
 use time::Instant;
@@ -26,20 +27,29 @@ fn try_solve(puzzle_str: &str, solution_str: &str) {
 }
 
 fn main() {
-    // File hosts must exist in current path before this produces output
-    let file = File::open("sudoku.csv").unwrap();
-    let reader = io::BufReader::new(file);
-    let lines = reader.lines();
+    let args: Vec<String> = env::args().collect();
 
+    let file = if args.len() > 1 { File::open(args[1].as_str()) } else { File::open("sudoku.csv")};
+    let reader = io::BufReader::new(file.unwrap());
+    let lines = reader.lines();
     let mut v_all: Vec<(String, String)> = Vec::new();
 
     for line in lines.skip(1) {
         let z = line.unwrap();
         let mut tokens = z.split(',');
-        v_all.push((
-            tokens.next().unwrap().to_string(),
-            tokens.next().unwrap().to_string(),
-        ));
+        let puzzle = tokens.next();
+        let solution = tokens.next();
+        match (puzzle, solution)
+        {
+            (Some(p), Some(s)) => {
+                v_all.push((
+                    p.to_string(),
+                    s.to_string(),
+                ));
+            }
+            _ => (), // ignore empty strings
+        }
+
     }
     let now = Instant::now();
 
